1c56cd32aa5f5af790689749fa0f183498910b55
SeongJJun <tjdwns@gmail.com>
2025-12-05 16:24:29 +0900

    first commit

---
+++ src/main/java/org/todayreading/book_collecting_worker/BookCollectingWorkerApplication.java
@@ -0,0 +1,13 @@
+package org.todayreading.book_collecting_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCollectingWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCollectingWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/todayreading/book_collecting_worker/BookCollectingWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.todayreading.book_collecting_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCollectingWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
