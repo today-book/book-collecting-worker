00d90637d5eb226eed59e27cf86b3329633dc1e9
SeongJJun <tjdwns@gmail.com>
2025-12-08 14:17:08 +0900

    [#13] Feat: NaverSearchInspectController 구현

--- src/main/java/org/todayreading/collectingworker/naver/presentation/controller/NaverSearchInspectController.java
+++ src/main/java/org/todayreading/collectingworker/naver/presentation/controller/NaverSearchInspectController.java
@@ -1,7 +1,6 @@
 package org.todayreading.collectingworker.naver.presentation.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
